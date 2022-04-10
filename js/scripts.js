270226c5e4b409d05f8206fd737c17ae93296da9
Alexander <knibz@protonmail.com>
2022-04-09 18:49:18 -0700

    add hide functions to clear answers

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,22 @@ $(document).ready(function() {
     if (input) {
       if (side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2) {
         $("#csharp").show();
+        $("#java").hide();
+        $("#python").hide();
       } else if (side1===side2 && side1===side3 && side2 === side3) {
         $("#java").show();
+        $("#python").hide();
+        $("#csharp").hide();
       } else if (side1===side2 || side1===side3 || side2 === side3) {
         $("#python").show();
+        $("#csharp").hide();
+        $("#java").hide();
       } else {
+        $("#csharp").hide();
+        $("#java").hide();
+        $("#python").hide();
         alert("Please enter a number and try again!")
+
       }
     }     
   });
