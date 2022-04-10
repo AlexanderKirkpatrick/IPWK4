8a84ff1e4c1a23c6193ba24dc02ff7bda3d82b0f
Alexander <knibz@protonmail.com>
2022-04-09 17:09:13 -0700

    fix functions, fix if else, add alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-  $("form#result").submit(function() {
-    const number = parseInt($("input#number").val());
-    const control = 10;
+  $("form#input").submit(function(event) {
+    event.preventDefault();    
+    const side1 = parseInt($("input#side1").val());
+    const side2 = parseInt($("input#side2").val());
+    const side3 = parseInt($("input#side3").val());
 
-    if (number >= control) {
-      $("#csharp").show();
-    } else if (number <= control) {
-      $("#java").show();
-    }
-
-    event.preventDefault();
+    if (input) {
+      if (side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2) {
+        $("#csharp").show();
+      } else if (side1===side2 && side1===side3 && side2 === side3) {
+        $("#java").show();
+      } else if (side1===side2 || side1===side3 || side2 === side3) {
+        $("#python").show();
+      } else {
+        alert("Please enter a number and try again!")
+      }
+    }     
   });
 });
